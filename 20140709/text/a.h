0a87be93b8af7857261ec041664937831b92d8cb
soul <souldong1591@163.com>
2014-07-09 11:33:01 +0800

    20140709

---
+++ 20140709/text/a.h
@@ -0,0 +1,8 @@
+#ifndef __A__
+#define __A__ 
+
+int a = 10;
+
+int add(int m, int n);
+
+#endif  /*__A__*/
---
+++ 20140709/text/main.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: Soul
+	> Mail:souldong1591@gmail.com 
+	> Created Time: 2014年07月09日 星期三 09时18分05秒
+ ************************************************************************/
+
+#include "a.h"
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int a = add(1, 2);
+    printf("%d\n", a);
+    return 0;
+}
